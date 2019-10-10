c72cd5e769f0802e8f5c9efb92eff0a2b3384890
Michael <q@mymac.local>
2019-10-10 15:51:50 -0400

    config MUI theme and add basic layout for homepage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 //Component Imports
 import Navbar from './components/Navbar';
-//Page Import
+//Page Imports
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#4f5b62',
+      main: '#263238',
+      dark: '#000a12',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#b0ff57',
+      main: '#76ff03',
+      dark: '#32cb00',
+      contrastText: '#000'
+    }
+  }
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home} />
-              <Route exact path="/login" component={login} />
-              <Route exact path="/signup" component={signup} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route exact path="/login" component={login} />
+                <Route exact path="/signup" component={signup} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 
 export class home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
+      <Grid container>
+        <Grid item sm={8} xs={12}>
+          <h1>Posts</h1>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <h1>Profile</h1>
+        </Grid>
+      </Grid>
     );
   }
 }
