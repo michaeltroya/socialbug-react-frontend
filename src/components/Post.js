cd0c34db9eb4c8472fcb81ae286a908a5f4eb16f
Michael <q@mymac.local>
2019-10-15 10:40:37 -0400

    import dayjs

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { Link } from 'react-router-dom/';
+//Dayjs imports
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 //Material UI Imports
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -23,6 +26,7 @@ const styles = {
 
 export class Post extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       post: { body, createdAt, userImage, userHandle, postId, likeCount, commentCount }
@@ -37,7 +41,7 @@ export class Post extends Component {
               {userHandle}
             </Typography>
             <Typography variant="body2" color="textSecondary">
-              {createdAt}
+              {dayjs(createdAt).fromNow()}
             </Typography>
             <Typography variant="body1">{body}</Typography>
           </CardContent>
