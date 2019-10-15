a6107acb8a1b54cc0ff71c2e923cd6ea891c76a2
Michael <q@mymac.local>
2019-10-15 10:53:31 -0400

    add temporary icon

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { Link } from 'react-router-dom/';
+import Icon from '../images/icon.png';
+//Material UI imports
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+
+const styles = {
+  form: {
+    textAlign: 'center'
+  }
+};
 
 export class login extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm></Grid>
+        <Grid item sm>
+          <p>HI</p>
+        </Grid>
+        <Grid item sm></Grid>
+      </Grid>
     );
   }
 }
 
-export default login;
+login.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(login);
