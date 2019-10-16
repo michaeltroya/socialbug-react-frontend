380f9203bedca3d55ee985426a2d6c99f368fa51
Michael <q@mymac.local>
2019-10-15 20:04:00 -0400

    add signup and logout user actions

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,4 +1,4 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types';
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from '../types';
 import axios from 'axios';
 
 export const loginUser = (userData, history) => dispatch => {
@@ -6,9 +6,7 @@ export const loginUser = (userData, history) => dispatch => {
   axios
     .post('/login', userData)
     .then(res => {
-      const FBIdToken = `Bearer ${res.data.token}`;
-      localStorage.setItem('FBIdToken', FBIdToken);
-      axios.defaults.headers.common['Authorization'] = FBIdToken;
+      setAuthHeader(res.data.token);
       dispatch(getUserData());
       dispatch({ type: CLEAR_ERRORS });
       history.push('/');
@@ -16,6 +14,27 @@ export const loginUser = (userData, history) => dispatch => {
     .catch(err => {
       dispatch({ type: SET_ERRORS, payload: err.response.data });
     });
+};
+
+export const signupUser = (createUserData, history) => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post('/signup', createUserData)
+    .then(res => {
+      setAuthHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push('/');
+    })
+    .catch(err => {
+      dispatch({ type: SET_ERRORS, payload: err.response.data });
+    });
+};
+
+export const logoutUser = () => dispatch => {
+  localStorage.removeItem('FBIdToken');
+  delete axios.defaults.headers.common['Authorization'];
+  dispatch({ type: SET_UNAUTHENTICATED });
 };
 
 export const getUserData = () => dispatch => {
@@ -30,4 +49,10 @@ export const getUserData = () => dispatch => {
     .catch(err => {
       console.log(err);
     });
+};
+
+const setAuthHeader = token => {
+  const FBIdToken = `Bearer ${token}`;
+  localStorage.setItem('FBIdToken', FBIdToken);
+  axios.defaults.headers.common['Authorization'] = FBIdToken;
 };
